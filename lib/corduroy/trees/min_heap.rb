78dc9eb77b0577a0f3b3ea046ba0055ce462b4c0
Andrew Hautau <arhautau@gmail.com>
2022-05-14 22:41:27 -0700

    implement heapify

--- lib/corduroy/trees/min_heap.rb
+++ lib/corduroy/trees/min_heap.rb
@@ -1,5 +1,6 @@
 # typed: true
 require "sorbet-runtime"
+require 'pry'
 
 module Corduroy
   module Trees
@@ -23,25 +24,17 @@ module Corduroy
         @array = Array.new(@max_size)
       end
 
-      sig { params(array: Array).returns(MinHeap) }
-      #
-      # Create a MinHeap instance from an Array of Comparables.
-      # The heap will have a max size of the array's length.
-      #
-      # @param [<Type>] array An array of Comparables.
-      #
-      # @return [<Type>] The MinHeap
-      #
-      def self.heapify(array:)
-        heap = MinHeap.new(max_size: array.length)
-        array.each do |val|
-          heap.insert(value: val)
-        end
-        heap
+      sig { params(array: Array).returns(Corduroy::Trees::MinHeap) }
+      def self.heapify(array)
+        min_heap = Corduroy::Trees::MinHeap.new(max_size: array.length)
+        min_heap.instance_variable_set(:@array, array)
+        min_heap.instance_variable_set(:@current_size, array.length)
+        min_heap.heapify_array(index: 0)
+        min_heap
       end
 
       sig { params(value: Integer).returns(TrueClass) }
-      def insert(value:)
+      def insert(value)
         if @current_size == @max_size
           raise MaxSizeError, "Cannot exceed maximum MinHeap size"
         end
@@ -55,7 +48,7 @@ module Corduroy
       sig { returns(Integer) }
       def extract!
         root = @array[0]
-        @array[0] = @array[@current_size]
+        @array[0] = @array[@current_size - 1]
         @current_size -= 1
         trickle_down(index: 0)
         root
@@ -79,6 +72,17 @@ module Corduroy
         true
       end
 
+      sig { params(index: Integer).void }
+      def heapify_array(index:)
+        if index > ((@current_size / 2) - 1)
+          # Don't do anything, we're at a node with no children
+        else
+          heapify_array(index: (index * 2) + 2)
+          heapify_array(index: (index * 2) + 1)
+          trickle_down(index: index)
+        end
+      end
+
       def to_s
         "#<Corduroy::Trees::MinHeap #{@array} >"
       end
@@ -88,10 +92,14 @@ module Corduroy
       # index is the index of the item we're removing
       sig { params(index: Integer).void }
       def trickle_down(index:)
+        # puts "tricket_down index => #{index}"
         smaller_child = 0
+        # root
         top = @array[index]
 
+        # while node has at least one child
         while index < @current_size / 2
+          # find the smaller child
           left_child = 2 * index + 1
           right_child = left_child + 1
 
@@ -101,11 +109,14 @@ module Corduroy
             smaller_child = left_child
           end
 
+          # top is smaller
           if top <= @array[smaller_child]
             break
           end
 
+          # shift child up
           @array[index] = @array[smaller_child]
+          # go down
           index = smaller_child
         end
 
--- test/corduroy/search/binary_search_test.rb
+++ test/corduroy/search/binary_search_test.rb
@@ -4,13 +4,13 @@ require "corduroy/search/binary_search"
 
 # A test of Corduroy::Search::BinarySearch
 class TestBinarySearch < Minitest::Test
-  def setup
-    @collection = (-100_000_000..100_000_000).to_a
-    @target = 8_765_309
-    @search = Corduroy::Search::BinarySearch.new(collection: @collection)
-  end
-
   def test_find
+    # NOTE: Uncomment to test a really big collection
+    # collection = (-100_000_000..100_000_000).to_a
+    # target = 8_765_309
+    # search = Corduroy::Search::BinarySearch.new(collection: collection)
+    # assert_equal 108_765_309, search.find(target: target)
+
     # empty collection
     assert_nil Corduroy::Search::BinarySearch.new(collection: [].to_a).find(target: 0)
 
@@ -18,7 +18,6 @@ class TestBinarySearch < Minitest::Test
     assert_equal 0, Corduroy::Search::BinarySearch.new(collection: [0].to_a).find(target: 0)
     assert_equal 7, Corduroy::Search::BinarySearch.new(collection: (1..10).to_a).find(target: 8)
     assert_equal 19, Corduroy::Search::BinarySearch.new(collection: (-11..10).to_a).find(target: 8)
-    assert_equal 108_765_309, @search.find(target: @target)
 
     # can't find things that aren't in collection
     assert_nil Corduroy::Search::BinarySearch.new(collection: (0..10).to_a).find(target: 999)
